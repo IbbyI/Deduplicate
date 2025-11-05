e4e23b031d461ea048988b450c3f671f8cc70cf6
Valor <ibbyissa001@gmail.com>
2025-11-05 21:43:20 +0100

    Refactor duplicate file detection logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
+import time
 import argparse
 from pathlib import Path
-from collections import Counter
 
 from utils.log import log
 from utils.hash import hash_file
@@ -34,7 +34,6 @@ def main(argv: list[str]) -> None:
         )
 
         args = parser.parse_args()
-        print(args)
         start_path = Path(args.path[0])
         duplicate_path = args.move_duplicates
 
@@ -50,17 +49,31 @@ def main(argv: list[str]) -> None:
         dict = {}
         for file in start_path.iterdir():
             hashed_file = hash_file(file)
-            dict.update({file: hashed_file})
+            dict.setdefault(hashed_file, []).append(file)
 
-        counts = Counter(dict.values())
-        result = {key: value for key, value in dict.items() if counts[value] > 1}
-        print(result.keys(), result.values())
+        duplicate_results = [value for value in dict.values() if len(value) > 1]
 
-    except argparse.ArgumentError and TypeError as e:
-        parser.print_help()
+        if not duplicate_results:
+            print(f"No Duplicates Found in {start_path}.\nTerminating Program...")
+            log(
+                level="info",
+                message=f"No Duplicates Found in {start_path}.\nTerminating Program...",
+            )
+            sys.exit()
+
+        for duplicates in duplicate_results:
+            paths = [str(p) for p in duplicates]
+            print(f"{', '.join(paths)} are duplicates.")
+            log(level="info", message=f"{', '.join(paths)} are duplicates.")
+
+    except argparse.ArgumentError or TypeError as e:
+        print(e)
         sys.exit(2)
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     log(level="info", message="Starting Program...")
     main(sys.argv[1:])
+    time_taken = time.time() - start_time
+    log(level="info", message=f"Time Taken: {"%.2f" % time_taken}s")
