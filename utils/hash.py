8dc392325f8c601f5c181f9f0f9539bb162997fa
Valor <ibbyissa001@gmail.com>
2025-11-08 13:23:36 +0100

    Filetype Argument for Hash Function Changed from str to  Path & Returns Value Error if Path is directory.

--- utils/hash.py
+++ utils/hash.py
@@ -1,17 +1,21 @@
 import hashlib
 from sys import exit
+from pathlib import Path
 
 from utils.log import log
 
 
-def hash_file(path: str) -> str:
+def hash_file(path: Path) -> str:
     """
     Compute SHA-256 Hash of File Contents in 4KB Chunks.
     Args:
-        path (str): Path of File
+        path (Path): Path to the file to hash
     Returns:
         str: SHA-256 Hash of File Contents
     """
+    if path.is_dir():
+        raise ValueError(f"Cannot hash a directory: {path}")
+
     try:
         log(level="info", message=f"Chunk Hashing Contents from File {path}.")
         sha256_hash = hashlib.sha256()
