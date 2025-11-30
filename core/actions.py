ea2039d4d028b0e14d6cefc23b8cc95d4510dd46
ibbyi <ibbyissa001@gmail.com>
2025-11-30 23:22:50 +0100

    Moved Verbose to UI Layer, Improved Verbose Decorator & Typechecking, & .csv Option Added for Output File

--- core/actions.py
+++ core/actions.py
@@ -2,31 +2,24 @@ import shutil
 from os import remove
 from pathlib import Path
 
-from ui.verbose import verbose
 
-
-@verbose(
-    lambda args, result: (
-        "No Results."
-        if result is None
-        else f"Moved: {len(result["moved"])}\n"
-        f"Skipped: {len(result["skipped"])}\n"
-        f"Skipped: {len(result["errors"])}"
-    )
-)
 def move_duplicates(
     duplicate_files: list[Path], move_path: Path, dry_run_flag: bool
-) -> dict[list[str | None]]:
+) -> dict[str, list[str | list[str | Exception]]]:
     """
     Move Duplicate Files to Given Directory.
     Args:
         duplicate_files (list[Path]): List of Duplicate Files Found.
         move_path (Path): Path to Move Duplicate Files to.
-        dry_run_flag (bool): Checks if Dry Run Flag is Enabled. False by Default.
+        dry_run_'flag (bool): Checks if Dry Run Flag is Enabled. False by Default.
     Returns:
         dict[list[str | None]]: Dictionary of All Files Moved, Skipped, or Failed to Move.
     """
-    result = {"moved": [], "skipped": [], "errors": []}
+    result: dict[str, list[str | list[str | Exception]]] = {
+        "moved": [],
+        "skipped": [],
+        "errors": [],
+    }
     for f in duplicate_files:
         if dry_run_flag:
             result["skipped"].append(str(f))
@@ -40,18 +33,9 @@ def move_duplicates(
     return result
 
 
-@verbose(
-    lambda args, result: (
-        "No Results."
-        if result is None
-        else f"Deleted: {len(result["deleted"])}\n"
-        f"Skipped: {len(result["skipped"])}\n"
-        f"Skipped: {len(result["errors"])}"
-    )
-)
 def delete_duplicates(
     duplicate_files: list[Path], dry_run_flag: bool
-) -> dict[list[str | None]]:
+) -> dict[str, list[str | list[str | Exception]]]:
     """
     Delete Duplicate Files.
     Args:
@@ -60,7 +44,11 @@ def delete_duplicates(
     Returns:
         dict[list[str | None]]: Dictionary of All Files Deleted, Skipped, or Failed to Delete.
     """
-    result = {"deleted": [], "skipped": [], "errors": []}
+    result: dict[str, list[str | list[str | Exception]]] = {
+        "deleted": [],
+        "skipped": [],
+        "errors": [],
+    }
     for f in duplicate_files:
         if dry_run_flag:
             result["skipped"].append(str(f))
@@ -74,14 +62,31 @@ def delete_duplicates(
     return result
 
 
-def write_to_output(duplicate_files: list[Path], output_file: Path) -> None:
+def write_to_output(
+    duplicate_files: list[Path], output_file: Path, file_extension: str
+) -> None:
     """
     Write Results to Output File.
     Args:
         duplicate_files (list[Path]): List of Duplicate Files Found.
         output_file (Path): Path of Output File to Write To.
     """
+    allowed_ext = [".txt", ".csv"]
     try:
+        if file_extension not in allowed_ext:
+            raise ValueError(
+                f"{file_extension} File Extension is not Supported For Output File."
+            )
+        if file_extension == ".csv":
+            import csv
+
+            with open(output_file, "w") as f:
+                csvwriter = csv.writer(f)
+
+                csvwriter.writerow(["Path"])
+                csvwriter.writerows([[str(p)] for p in duplicate_files])
+            return
+
         with open(output_file, "w") as f:
             f.write("✅ Duplicate Files Found:\n")
             for file in duplicate_files:
