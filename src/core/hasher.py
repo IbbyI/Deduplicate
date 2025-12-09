98e12145c879902b5de23d892792821d737d6053
ibbyi <ibbyissa001@gmail.com>
2025-12-09 22:07:56 +0100

    Removed Magic Numbers to Constant Variable & Improved Error Checking

--- src/core/hasher.py
+++ src/core/hasher.py
@@ -1,48 +1,53 @@
 import hashlib
 from pathlib import Path
 
-from ui.verbose import verbose
+
+QUICK_HASH_BYTES_THRESHOLD: int = 2 * 1024 * 1024
 
 
-@verbose(lambda args, file: f"Full Hashing File: {args[0]}")
-def full_hash(path: Path) -> str:
+def full_hash(file: Path) -> str:
     """
     Compute SHA-256 Hash of Entire Contents of File in 4KB Chunks.
     Args:
-        path (Path): Path to the File to Hash
+        file (Path): Path to the File to Hash
     Returns:
         str: SHA-256 Hash of File Contents
     """
-    if path.is_dir():
-        raise ValueError(f"❌ Cannot hash a directory: {path}")
+    if file.is_dir():
+        raise ValueError(f"❌ Cannot hash a directory: {file}")
+
+    if not file.exists():
+        raise FileNotFoundError(f"❌ {file} does not exist.")
 
     try:
         sha256_hash = hashlib.sha256()
-        with open(path, "rb") as f:
+        with open(file, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 sha256_hash.update(chunk)
     except Exception as e:
-        raise RuntimeError(f"❌ Failed to hash file '{path}': {e}") from e
+        raise RuntimeError(f"❌ Failed to hash file '{file}': {e}") from e
     return sha256_hash.hexdigest()
 
 
-@verbose(lambda args, file: f"Quick Hashing File: {args[0]}")
-def quick_hash(path: Path) -> str:
+def quick_hash(file: Path) -> str:
     """
     Compute 4KB Chunks from Start and End of file using SHA-256 Hash of File Contents.
     Args:
-        path (Path): Path to the File to Hash
+        file (Path): Path to the File to Hash
     Returns:
         str: SHA-256 Hash of Sampled File Contents
     """
-    if path.is_dir():
-        raise ValueError(f"❌ Cannot hash a directory: {path}")
+    if file.is_dir():
+        raise ValueError(f"❌ Cannot hash a directory: {file}")
+
+    if not file.exists():
+        raise FileNotFoundError(f"❌ {file} does not exist.")
 
     try:
         sha256_hash = hashlib.sha256()
-        file_size = path.stat().st_size
+        file_size = file.stat().st_size
 
-        with open(path, "rb") as f:
+        with open(file, "rb") as f:
             sha256_hash.update(f.read(4096))
             if file_size > 4096:
                 f.seek(-4096, 2)
@@ -51,22 +56,21 @@ def quick_hash(path: Path) -> str:
         return sha256_hash.hexdigest()
 
     except Exception as e:
-        raise RuntimeError(f"❌ Failed to hash file '{path}': {e}") from e
+        raise RuntimeError(f"❌ Failed to hash file '{file}': {e}") from e
 
 
-@verbose(lambda args, file: f"Auto Hashing File: {args[0]}")
-def auto_hash(path: Path) -> str:
+def auto_hash(file: Path) -> str:
     """
     Compute SHA-256 Hash of File Contents Based on File Size.
     If The File > 2MB, It Fast Hashes the File, Else Slow Hash.
         Args:
-        path (Path): Path o the file to hash
+        file (Path): Path oF the file to hash
     Returns:
         str: SHA-256 Hash of File Contents or Sample of File.
     """
     try:
-        if path.stat().st_size <= 2 * 1024 * 1024:
-            return full_hash(path)
+        if file.stat().st_size <= QUICK_HASH_BYTES_THRESHOLD:
+            return full_hash(file)
+        return quick_hash(file)
     except Exception as e:
-        raise RuntimeError(f"❌ Failed to hash file '{path}': {e}") from e
-    return quick_hash(path)
+        raise RuntimeError(f"❌ Failed to hash file '{file}': {e}") from e
