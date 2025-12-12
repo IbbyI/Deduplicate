594781b5b71108fff90f3836ea47b3de5954f683
ibbyi <ibbyissa001@gmail.com>
2025-12-12 12:50:58 +0000

    Seperated Output FIle UI From Actions UI & Fixed String Formatting for Results Dictionary Keys

--- src/ui/adapter_actions.py
+++ src/ui/adapter_actions.py
@@ -1,26 +1,19 @@
 from pathlib import Path
 
-from os.path import splitext
-
 from ui.verbose import verbose
 from ui.display import ask_yes_no, success, warn, error
 
 from core.log import log
-from core.actions import (
-    write_to_output,
-    delete_duplicates,
-    move_duplicates,
-    output_file_format,
-)
+from core.actions import delete_duplicates, move_duplicates
 
 
 @verbose(
     lambda args, result: (
         "No Results."
         if result is None
-        else f"Moved: {len(result["moved"])}\n"
-        f"Skipped: {len(result["skipped"])}\n"
-        f"Skipped: {len(result["errors"])}"
+        else f"Moved: {len(result['moved'])}\n"
+        f"Skipped: {len(result['skipped'])}\n"
+        f"Skipped: {len(result['errors'])}"
     )
 )
 def handle_move(
@@ -40,13 +33,13 @@ def handle_move(
     )
     log(
         level="info",
-        message=f"Moved: {result["moved"]}, skipped: {result["skipped"]}, errors: {result["errors"]}",
+        message=f"Moved: {result['moved']}, skipped: {result['skipped']}, errors: {result['errors']}",
     )
 
-    success(f"Moved: {len(result["moved"])} Files.", style="")
-    warn(f"Skipped: {len(result["skipped"])} Files.", style="")
+    success(f"Moved: {len(result['moved'])} Files.", style="")
+    warn(f"Skipped: {len(result['skipped'])} Files.", style="")
     if result["errors"]:
-        error(f"Errors: {len(result["errors"])} Files.", style="")
+        error(f"Errors: {len(result['errors'])} Files.", style="")
     return result
 
 
@@ -54,9 +47,9 @@ def handle_move(
     lambda args, result: (
         "No Results."
         if result is None
-        else f"Deleted: {len(result["deleted"])}\n"
-        f"Skipped: {len(result["skipped"])}\n"
-        f"Skipped: {len(result["errors"])}"
+        else f"Deleted: {len(result['deleted'])}\n"
+        f"Skipped: {len(result['skipped'])}\n"
+        f"Skipped: {len(result['errors'])}"
     )
 )
 def handle_delete(
@@ -75,38 +68,13 @@ def handle_delete(
     )
     log(
         level="info",
-        message=f"Deleted: {result["deleted"]}, skipped: {result["skipped"]}, errors: {result["errors"]}",
+        message=f"Deleted: {result["deleted"]}, skipped: {result['skipped']}, errors: {result['errors']}",
     )
-    success(f"Deleted {len(result["deleted"])} Files.")
-    warn(f"Skipped: {len(result["skipped"])} Files.")
+    success(f"Deleted {len(result['deleted'])} Files.")
+    warn(f"Skipped: {len(result['skipped'])} Files.")
     if result["errors"]:
-        error(f"Errors: {len(result["errors"])} Files.")
+        error(f"Errors: {len(result['errors'])} Files.")
     return result
-
-
-@verbose(
-    lambda args, result: f"Output File Set as {args[1]}, Duplicate Files Passed: {True if args[0] else False}"
-)
-def handle_output_file(duplicate_files: list[Path], output_file: Path) -> None:
-    """
-    Handles UI to Write to Output File.
-    Args:
-        duplicate_files (list[Path]): List of Duplicate Files Found.
-        output_file (Path): Path of Output File to Write To.
-    """
-    try:
-        file_extension = splitext(output_file)[1]
-        output_file_data = output_file_format(duplicate_files)
-        write_to_output(output_file_data, output_file, file_extension)
-        success(
-            f"Sucessfully Wrote to Output File: {output_file}", style="bold underline"
-        )
-    except IOError as e:
-        log(
-            level="error",
-            message=f"❌ Failed To Write to Output File: {output_file}, {e}",
-        )
-        raise IOError(f"❌ Failed To Write to Output File: {output_file}, {e}") from e
 
 
 @verbose(lambda args, confirm: f"Requesting Confirmation from User. {confirm}")
