ceb9b5d8afc6b13631c5966ae0d62c3ef3164fce
ibbyi <ibbyissa001@gmail.com>
2025-12-12 12:53:58 +0000

    Fix Log Message Assertions to Include Expected Log Level

--- tests/test_log.py
+++ tests/test_log.py
@@ -9,16 +9,16 @@ class TestLogFunction(unittest.TestCase):
             log(level="info", message="Info Level Test Case.")
         self.assertEqual(len(l.output), 1)
         self.assertIn("INFO:core.log:Info Level Test Case.", l.output[0])
-        
+
         with self.assertLogs(logger, level="WARNING") as l:
             log(level="warning", message="Warning Level Test Case.")
         self.assertEqual(len(l.output), 1)
-        self.assertIn("Warning Level Test Case.", l.output[0])
-        
+        self.assertIn("WARNING:core.log:Warning Level Test Case.", l.output[0])
+
         with self.assertLogs(logger, level="ERROR") as l:
             log(level="error", message="Error Level Test Case.")
         self.assertEqual(len(l.output), 1)
-        self.assertIn("Error Level Test Case.", l.output[0])
+        self.assertIn("ERROR:core.log:Error Level Test Case.", l.output[0])
 
 
 if __name__ == "__main__":
