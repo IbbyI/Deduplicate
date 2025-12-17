a56250555e4c42ccf3652c187b9ad385d82d137a
ibbyi <ibbyissa001@gmail.com>
2025-12-17 10:21:50 +0000

    Updated Version Number

--- src/cli/dedupe.py
+++ src/cli/dedupe.py
@@ -30,7 +30,7 @@ def build_parser() -> argparse.Namespace:
         "-VER",
         "--version",
         action="version",
-        version="Deduplicate 1.2.6",
+        version="Deduplicate 1.2.7",
         help="Show Program Version.",
     )
     parser.add_argument(
