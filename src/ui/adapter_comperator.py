f3e213a486b0de73afbe9ba164afccf89c3a4ba0
ibbyi <ibbyissa001@gmail.com>
2025-12-17 10:20:28 +0000

    Added Transient Parameter To Remove Progess Bars on Task Completion

--- src/ui/adapter_comperator.py
+++ src/ui/adapter_comperator.py
@@ -5,7 +5,7 @@ from core.log import log
 from core.comperator import compare_files
 
 from ui.verbose import verbose
-from ui.display import ask_yes_no, print_duplicates, error, info
+from ui.display import ask_yes_no, print_duplicates, error, success
 
 progress = Progress()
 
@@ -21,18 +21,16 @@ def compare_files_ui(
     Returns:
         list[Path]: List of duplicate files, with the oldest file removed from each group.
     """
-    progress.start()
     try:
-        progress.add_task("[purple]Comparing Files            ", total=None)
-        log(level="info", message="Comparing Files")
-        result = compare_files(duplicate_files, keep_newest_file)
-        return result
+        with Progress(transient=True) as progress:
+            progress.add_task("[purple]Comparing Files            ", total=None)
+            log(level="info", message="Comparing Files")
+            result = compare_files(duplicate_files, keep_newest_file)
+            return result
     except Exception as e:
         error(str(e))
         log(level="error", message=str(e))
         return []
-    finally:
-        progress.stop()
 
 
 def print_total_duplicates(result: list[Path]) -> None:
@@ -42,12 +40,12 @@ def print_total_duplicates(result: list[Path]) -> None:
         list[Path]: List of duplicate files.
     """
     number_of_duplicates = len(result)
-    info(f"{number_of_duplicates} Duplicates Found.", style="bold underline")
+    success(f"{number_of_duplicates} Duplicates Found.", style="bold underline")
     log(level="info", message=f"{number_of_duplicates} Duplicates Found.")
     if number_of_duplicates > 30:
         if not ask_yes_no(
             f"Print All {number_of_duplicates} Duplicate File Paths?",
-            style="bold underline red",
+            style="bold underline green",
         ):
             return
     print_duplicates(result)
